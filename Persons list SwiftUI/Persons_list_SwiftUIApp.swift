3e3fc872e71f8476da592fccb63b7d306c887e23
Marat <Shagiakhmetov2499@gmail.com>
2021-06-15 12:31:01 +0300

    Initial Commit

---
+++ Persons list SwiftUI/Persons_list_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Persons_list_SwiftUIApp.swift
+//  Persons list SwiftUI
+//
+//  Created by Marat Shagiakhmetov on 15.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Persons_list_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
